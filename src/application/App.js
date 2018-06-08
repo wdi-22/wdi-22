4b2d03d4c0383932cc820e36de2ed06d3769941a
briankeegan <brian.morris.keegan@gmail.com>
2018-06-08 10:29:17 -0400

    Create Footer
    Update css accordingly
    Add to application

--- src/application/App.js
+++ src/application/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Nav from '../navbar/Nav'
 import Home from '../homepage/Home'
+import Footer from '../footer/Footer'
 import './App.css'
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <Home />
+        <Footer />
       </div>
     )
   }
